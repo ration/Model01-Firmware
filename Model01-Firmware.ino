f77dd4886720d0ab5dc65aa31deb0099c54a55a2
Lasse Lindgård <lasse@lldata.dk>
2017-10-29 19:44:28 +0100

    moved mods
    reach less for shift and allow for easier chording with ctrl

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -147,15 +147,15 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
   (Key_Escape,   Key_1, Key_2, Key_3, Key_4, Key_5, Key_LeftBracket,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftGui,
-   OSM(LeftControl), Key_Backspace, OSM(LeftGui), OSM(LeftShift),
+   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, OSM(LeftGui),
+   OSM(LeftAlt), Key_Backspace, OSM(LeftShift), OSM(LeftControl),
    ShiftToLayer(FUNCTION),
 
    Key_RightBracket,  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_KeypadNumLock,
    Key_Enter,         Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                       Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    OSM(RightGui),     Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   OSM(RightShift),OSM(LeftAlt),  Key_Spacebar, OSM(RightControl),
+   OSM(RightControl), OSM(LeftShift),  Key_Spacebar, OSM(RightAlt),
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] =  KEYMAP_STACKED
