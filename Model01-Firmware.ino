3bde7cf419b25a45172f6514beb23de3f1710e8c
Lasse Lindgård <lasse@lldata.dk>
2018-02-04 21:14:30 +0100

    right fn LockLayer(FUNCTION)
    since I rarely use the right fn, I seems useful to map that to lock to the function layer

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -168,16 +168,16 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
   (Key_Escape,   Key_1, Key_2, Key_3, Key_4, Key_5, Key_LeftBracket,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   MT(RightGui, PageDown), Key_Z, Key_X, Key_C, Key_V, Key_B, LockLayer(FUNCTION),
+   MT(LeftGui, PageDown), Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftBracket,
    OSM(LeftAlt), Key_Backspace, OSM(LeftShift), OSM(LeftControl),
    ShiftToLayer(FUNCTION),
 
    Key_RightBracket,  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,         Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                       Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_LeftGui,       Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   OSM(RightControl), OSM(LeftShift),  Key_Spacebar, Key_RightAlt,
-   ShiftToLayer(FUNCTION)),
+   Key_RightBracket,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   OSM(RightControl), OSM(LeftShift),  Key_Spacebar, Key_RightAlt, // Key_RightAlt is not oneshot, since wincompose assumes you tap and release the wincompose key
+   LockLayer(FUNCTION)),
 
   [NUMPAD] =  KEYMAP_STACKED
   (___, ___, ___, ___, ___, ___, Key_LEDEffectNext,
